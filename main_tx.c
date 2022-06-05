ff9afacace4d7f944967a571991067cca5a4a54e
JeongKyunPark <park32323@gmail.com>
2022-06-05 12:44:35 +0900

    Divide rx&tx module
    - use corresponding module for build

---
+++ main_tx.c
@@ -0,0 +1,19 @@
+#include <stm32f10x.h>
+#include "prj.h"
+
+u8 current_data = 0;
+u32 matrix_col = 0x40000000; // This address should be revised.
+
+int main(){
+	//Mr.kim fighting!!
+	//Mr.park fighting!!
+	//yeah~
+	
+	//Activte Dot matrix & Keypad, we follow connection of Chapter9 - step9
+	enable_dot_matrix();
+	enable_keypad();
+	enable_TMP();
+	
+	/* Loop waiting for interrupt */
+	while(1){ __WFI(); }
+}
