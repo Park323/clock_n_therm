f0be619f119190366453310b370b6e04c38e5f0f
JeongKyunPark <park32323@gmail.com>
2022-06-07 00:31:11 +0900

    Update prj.h
    - added some variables & functions
    - better visibility for rawdata

--- prj.h
+++ prj.h
@@ -1,3 +1,4 @@
+#include <stm32f10x.h>
 #ifndef DEF_HEADER    // include guard
 #define DEF_HEADER    // prevents the file from being included twice.
 #define MATRIX_COL		0x4001100C
@@ -5,8 +6,16 @@
 
 u8 cur_data;
 u16 cur_TMP;
+u8 hour, min, sec;
 u8 font8x8[16][8];
-uint64_t rawdata[8] = {0x3c103c3c087e, 0x423042421840, 0x46500202287c, 0x4a100c1c4802, 0x52103002fe02, 0x621042420842, 0x3c7c7e3c1c3c, 0x000000000000 }; // Data information to display. 
+uint64_t rawdata[8] = {0x3c103c3c087e, 
+											 0x423042421840, 
+											 0x46500202287c, 
+											 0x4a100c1c4802, 
+											 0x52103002fe02, 
+											 0x621042420842, 
+											 0x3c7c7e3c1c3c, 
+											 0x000000000000 }; // Data information to display. 
 
 void enable_dot_matrix(void);
 
@@ -14,20 +23,26 @@ void enable_keypad(void);
 
 void enable_TMP(void);
 
+void enable_clk(void);
+
 void enable_TIM1(void);
 
 void enable_TIM2(void);
 
 void enable_TIM3(void);
 
-void print_font();
+void print_font(void);
 
-void tmp2data();
+void tmp2data(void);
 
-void tmp2data_off();
+void tmp2data_off(void);
 
-void clk2data ();
+void display_clock_on(void);
+void display_clock_off(void);
 
-void clk2data_off ();
+void switch_clk_config(void);
+void switch_12_24(void);
+
+void updown_clock(u8 command);
 
 #endif
