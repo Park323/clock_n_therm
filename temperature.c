8b85c483de38a93f27e583acb048c8de6b399041
cacio-pe <jini6644@gmail.com>
2022-06-06 15:45:48 +0900

    tmp2data
    러프하게 짜봤어요

--- temperature.c
+++ temperature.c
@@ -35,8 +35,36 @@ void enable_TMP(){
 	ADC1->CR2 |= 1 << 22; //SWSTART
 }
 
-void tmp2data() {
+void tmp2data() {						//use TIM4. TIM4 causes update interrupt. ADC_DR is converted to rawdata.
+	/* enable TIM4 */
+	/* TIM4 is used for update 'rawdata' with temparature */
+	RCC->APB1ENR |= 1 << 3; //enable TIM4
+	
+	TIM4->CR1 = 0x00;
+	TIM4->CR2 = 0x00;
+	TIM4->PSC = 0x10;	
+	TIM4->ARR = 0x2000;				//adjustment required.
+	
+	TIM4->DIER |= 1;					//update interrupt enable.
+	NVIC->ISER[0] |= 1 <<30;	//TIM4 global interrupt enable.
+	
+	//activate clock
+	TIM4->CR1 |= 0x0001;
 }
 
 void tmp2data_off() {
-	TIM3->CR1 |= 0x0001;
+	TIM4->CR1 &= ~(1<<0);			//TIM4 disabled.
+}
+
+void TIM4_IRQHandler (void){
+	if ((TIM4->SR & 0x0001) != 0){
+		temp_cel = cur_TMP;			//Celsius temparature. round down after devide.
+		temp_cel_10 = temp_cel/10		//tens of celsius temp.
+		temp_cel_1 = temp_cel-(10*temp_cel_10);		//units of celsius temp.
+		
+		for(i=0, i<8, i++) {
+			rawdata[i] = (font8x8[temp_cel_10][i]*0x10000)+(font8x8[temp_cel_1][i]*0x100)+font8x8[0xC][i]			//nnC type
+			rawdata[i] = rawdata[i]*0x1000000 + rawdata[i]				//nnCnnC type
+		}
+	TIM4->SR &= ~(1<<0);		//clear UIF
+}
